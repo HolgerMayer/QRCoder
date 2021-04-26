bd6cb51679e545d3195c7c08e76aee2c61903c3a
Holger Mayer <holgermayer@icloud.com>
2021-04-26 09:51:40 +0200

    Initial Commit

---
+++ QRCoder/QRCoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCoderApp.swift
+//  QRCoder
+//
+//  Created by Holger Mayer on 26.04.21.
+//
+
+import SwiftUI
+
+@main
+struct QRCoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
