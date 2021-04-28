960592469b48b28719b96a035ba3826d06f68443
Holger Mayer <holgermayer@icloud.com>
2021-04-28 19:33:27 +0200

    Label unter QRCode beim Drucken eingeführt

--- QRCoder/Views/CreateQRCodeView.swift
+++ QRCoder/Views/CreateQRCodeView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import UIKit
+import CoreGraphics
 
 struct CreateQRCodeView: View {
     @State var contentText : String = ""
@@ -45,9 +46,50 @@ struct CreateQRCodeView: View {
         // Erst durch pndData() wird das Bild des QR-Codes erzeugt.
         // Sonst wird nichts angezeigt.
         //
-        let uiImage = UIImage(ciImage: image!)
+        let qrImage = UIImage(ciImage: image!)
+        
+        return UIImage(data:qrImage.pngData()!)
+    }
+    
+    func expandQR() -> UIImage? {
+        guard let qrImage = qrImage else { return nil }
+        let targetSize = CGSize(width: qrImage.size.width, height: qrImage.size.height * 2.0)
+        UIGraphicsBeginImageContextWithOptions(targetSize, true, 0.0)
+            // get context
+        guard let context: CGContext = UIGraphicsGetCurrentContext() else { return nil
+            
+        }
+        
+        context.setFillColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0);
+        context.fill(CGRect(origin: CGPoint(x: 0.0, y: 0.0), size: targetSize));
+            // push context to make it current
+            // (need to do this manually because we are not drawing in a UIView)
+        UIGraphicsPushContext(context)
+        // drawing code comes here- look at CGContext reference
+        // for available operations
+        // this example draws the inputImage into the context
+        let qrRect = CGRect(origin: CGPoint(x: 0.0, y: 0.0), size: qrImage.size)
+        qrImage.draw(in: qrRect)
+        // pop context
+        UIGraphicsPopContext()
+        
+        let string = contentText
+        let textTargetSize: CGSize = CGSize(width: targetSize.width, height: targetSize.height / 2.0)
+        let origin: CGPoint =  CGPoint(x: 0.0, y: targetSize.height / 2)
+        let font: UIFont = UIFont.boldSystemFont(ofSize: 10)
 
-        return UIImage(data:uiImage.pngData()!)
+        let attrs: [NSAttributedString.Key:Any] = [.font: font]
+        let boundingRect = string.boundingRect(with: targetSize, options: [.usesLineFragmentOrigin], attributes: attrs, context: nil)
+        let textRect = CGRect(origin: origin, size: boundingRect.size)
+        string.draw(with: textRect, options: [.usesLineFragmentOrigin], attributes: attrs, context: nil)
+            // get a UIImage from the image context- enjoy!!!
+        guard let outputImage: UIImage = UIGraphicsGetImageFromCurrentImageContext() else {
+            return nil
+        }
+        // clean up drawing environment
+        UIGraphicsEndImageContext()
+        
+        return UIImage(data:outputImage.pngData()!)
     }
     
     func printQR() {
@@ -61,7 +103,7 @@ struct CreateQRCodeView: View {
         printInfo.jobName = "myPrintJob"
         printController.printInfo = printInfo
                     
-        printController.printingItem = self.qrImage
+        printController.printingItem = expandQR()
 
         // Present print controller like usual view controller. Also completionHandler is available if needed.
         printController.present(animated: true)
